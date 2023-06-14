71a14f7c513f8829ee6497a9c81c1c4a1e1f6db8
mioyday <mioyday@gmail.com>
2023-06-14 22:09:55 +0900

    20ND_스프링 DB 접근 기술

--- src/main/java/hello/hellospring/repository/JdbcTemplateMemberRepository.java
+++ src/main/java/hello/hellospring/repository/JdbcTemplateMemberRepository.java
@@ -45,6 +45,7 @@ public class JdbcTemplateMemberRepository implements MemberRepository {
     @Override
     public Optional<Member> findByName(String name) {
         List<Member> result = jdbcTemplate.query("select * from member where name = ?", memberRowMapper(), name);
+
         return result.stream().findAny();
     }
     private RowMapper<Member> memberRowMapper() {

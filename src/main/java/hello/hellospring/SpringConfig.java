e22a779d4b1700203a4a94d6f75f0097a9e9316f
mioyday <mioyday@gmail.com>
2023-06-17 21:55:33 +0900

    22ND_스프링 DB 접근 기술

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,26 +1,40 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
-    private final DataSource dataSource;
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+
+    private  final  MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
-    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
+
+    //private EntityManager em;
+    //private final DataSource dataSource;
+
+  ///  @Autowired
+  //  public SpringConfig(DataSource dataSource) {
+
+  //      this.dataSource = dataSource;
+  //  }
+//    @Bean
+//    public MemberService memberService() {
+//
+//        return new MemberService(memberRepository());
+//    }
     @Bean
     public MemberRepository memberRepository() {
 // return new MemoryMemberRepository();
 // return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
-    }//
+// return new JdbcTemplateMemberRepository(dataSource);
+   return new JpaMemberRepository(em);
+    }
 }
